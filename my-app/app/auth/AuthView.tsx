0f183a560f7c3311c516b44eb28223d4ad80c712
Rohan-756 <rohansuresh2005@gmail.com>
2025-11-02 14:31:12 +0530

    Removed reduntant files and improved project structure

--- my-app/app/auth/AuthView.tsx
+++ my-app/app/auth/AuthView.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from "react";
-import Header from "./Header";
+import Header from "../components/Header";
 import { useRouter} from "next/navigation"
 
 export default function AuthView({handlePasswordReset, setView, setIsLoading, setUser, onAuthSuccess }: any) {
--- my-app/app/auth/page.tsx
+++ my-app/app/auth/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import AuthView from "../components/AuthView";
+import AuthView from "./AuthView";
 
 export default function AuthPage() {
   const router = useRouter();
